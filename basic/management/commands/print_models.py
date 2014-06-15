bb8acb37c36f14c8bbf635e470b8c24868107b78
Max Yurlov <maxyurloff@gmail.com>
2014-06-15 22:56:06 +0300

    #Create django command that prints all project models and the count of objects in every model

---
+++ basic/management/commands/print_models.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models.loading import get_models
+from sys import stderr
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+    	models = "Project models:\n%s" % \
+        "\n".join(["%s - %s" % (m._meta.object_name, m.objects.count())
+                   for m in get_models()])
+        
+        print >> stderr, models
+        return models
+        
--- basic/tests.py
+++ basic/tests.py
@@ -1,7 +1,13 @@
 from django.test import Client, TestCase
+from django.core.management import call_command
+from django.template import Context, Template
 from django.core.urlresolvers import reverse
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.db.models.loading import get_models
+from datetime import date
+import sys
+import os
 
 from basic.models import Person, Location
 
@@ -94,5 +100,32 @@ class BasicTest(TestCase):
 		self.failUnlessEqual(data['email'], p.email)
 		self.failUnlessEqual(data['jabber'], p.jabber)
 		self.failUnlessEqual(data['date'], str(p.date))
-		self.failUnlessEqual(data['contacts'], p.contacts)		
+		self.failUnlessEqual(data['contacts'], p.contacts)	
 		
+	def test_tag_edit_link(self):
+		response = Template("{% load basic_urls %}{% edit_link user %}").render(
+		    Context({"user": self.user}))
+		c = self.user.__class__
+		self.failUnlessEqual(response, reverse('admin:%s_%s_change' %  \
+		    (c._meta.app_label, c._meta.module_name), args=[self.user.pk]))
+	
+	def test_print_models(self):
+		sys.stdout = open('dump_mycommand.txt', 'w')
+		call_command('print_models')
+		sys.stdout.close()
+		sys.stdout = sys.__stdout__
+		
+		f = file('dump_mycommand.txt', "r")
+		result = "Project models:\n%s" % \
+		    "\n".join(["%s - %s" % (m._meta.object_name, m.objects.count())
+		                 for m in get_models()])
+		self.failUnlessEqual(f.read().strip('\n'), result.strip('\n'))
+		f.close()
+		os.remove('dump_mycommand.txt')
+		
+	def test_bash_script(self):
+		os.system('./print_models')
+		file = './%s.dat' % date.today().strftime("%Y-%m-%d")
+		self.failUnlessEqual(
+			os.path.exists('./%s.dat' % date.today().strftime("%Y-%m-%d")), True)
+		os.remove(file)
