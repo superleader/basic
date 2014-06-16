55dc523dbd2187819157af7b304e61eb6b6bc4df
Max Yurlov <maxyurloff@gmail.com>
2014-06-16 12:30:56 +0300

    corrected ticket #9

--- basic/management/commands/print_models.py
+++ basic/management/commands/print_models.py
@@ -5,7 +5,7 @@ from sys import stderr
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-    	models = "Project models:\n%s" % \
+    	models = "error: Project models:\n%s" % \
         "\n".join(["%s - %s" % (m._meta.object_name, m.objects.count())
                    for m in get_models()])
         
